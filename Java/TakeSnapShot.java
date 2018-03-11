a10923ac3d152b761a007598edbbe0d37b0214f3
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-11 08:28:39 -0400

    Added snapshot java script

---
+++ Java/TakeSnapShot.java
@@ -0,0 +1,21 @@
+import javax.imageio.*;
+
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import com.github.sarxos.webcam.Webcam;
+public class TakeSnapShot {
+
+    public static void main(String[] args) throws IOException{
+		
+	Webcam w = Webcam.getDefault();
+	w.getDevice().setResolution(new Dimension(1280,720));
+	w.open();
+
+	String desitination_filename = "Snapshot.png";
+	ImageIO.write(w.getImage(), "png", new File( destination_filename ));
+	w.close();
+		
+    }
+	
+}
