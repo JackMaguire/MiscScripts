7b3d8c67fd9672b4a1043dbe940a227191dd2e3b
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-28 15:18:57 -0400

    Added angle1_to_x()

--- Blender/MLHBondsClashes/draw_S_S_box.py
+++ Blender/MLHBondsClashes/draw_S_S_box.py
@@ -5,25 +5,30 @@ import numpy
 
 def my_assert_equals( name, actual, theoretical ):
     if actual != theoretical:
-        print( name + " is equal to " + actual + " instead of " + theoretical )
+        print( str( name ) + " is equal to " + str( actual ) + " instead of " + str( theoretical ) )
         exit( 1 )
 
 
 # 2*pi 
 #N_SLOTS_X = 71
-SLOT_WIDTH_X = 0.1 #angle1
-SLOT_WIDTH_Y = 0.1 #angle2
+#SLOT_WIDTH_X = 0.1 #angle1
+#SLOT_WIDTH_Y = 0.1 #angle2
 SLOT_WIDTH_Z = 0.5 #dist
 
 def angle1_to_x( angle1 ):
-    return math.floor( angle*10 ) / 10;
+    if( angle1 < 0 ):
+        return math.floor( angle1 * 10 ) / 10
+    else:
+        return math.floor( angle1 * 10 ) / 10
 
-my_assert_equals( "angle1_to_x( 0 )", angle1_to_x( 0 ), 0 )
-my_assert_equals( "angle1_to_x( -0.1 )", angle1_to_x( -0.1 ), -1 )
-my_assert_equals( "angle1_to_x( 0.1 )", angle1_to_x( 0.1 ), 0 )
-my_assert_equals( "angle1_to_x( 0.99 )", angle1_to_x( 0.99 ), 0 )
-my_assert_equals( "angle1_to_x( 1.00 )", angle1_to_x( 1.00 ), 1 )
-exit( 0 )
+my_assert_equals( "angle1_to_x( 0 )",     angle1_to_x( 0 ),      0   )
+my_assert_equals( "angle1_to_x( -0.11 )", angle1_to_x( -0.11 ), -0.2 )
+my_assert_equals( "angle1_to_x( -0.1 )",  angle1_to_x( -0.1 ),  -0.1 )
+my_assert_equals( "angle1_to_x( -0.09 )", angle1_to_x( -0.09 ), -0.1 )
+my_assert_equals( "angle1_to_x( 0.1 )",   angle1_to_x( 0.1 ),    0.1 )
+my_assert_equals( "angle1_to_x( 0.099 )", angle1_to_x( 0.099 ),  0   )
+my_assert_equals( "angle1_to_x( 0.99 )",  angle1_to_x( 0.99 ),   0.9 )
+my_assert_equals( "angle1_to_x( 1.00 )",  angle1_to_x( 1.00 ),   1.0 )
 
 def create_cube( x, y, z, listr ):
     bpy.ops.mesh.primitive_cylinder_add(
