bba7c9ac8b7b75b8216fa7de9c805e8fc8b550bf
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-28 15:37:22 -0400

    added global data

--- Blender/MLHBondsClashes/draw_S_S_box.py
+++ Blender/MLHBondsClashes/draw_S_S_box.py
@@ -1,7 +1,16 @@
-#import bpy
+import bpy
 import math
 import numpy
-#import re
+import re
+
+# Global data #
+all_boxes = []
+
+box_hist = []
+for x in range( 0, 10 ):
+    box_hist.append( [] )
+
+
 
 def my_assert_equals( name, actual, theoretical ):
     if actual != theoretical:
@@ -13,7 +22,11 @@ def my_assert_equals( name, actual, theoretical ):
 #N_SLOTS_X = 71
 #SLOT_WIDTH_X = 0.1 #angle1
 #SLOT_WIDTH_Y = 0.1 #angle2
-SLOT_WIDTH_Z = 0.5 #dist
+#SLOT_WIDTH_Z = 0.5 #dist
+
+BOX_X = 0.1
+BOX_Y = 0.1
+BOX_Z = 0.1
 
 def angle1_to_x( angle1 ):
     return math.floor( angle1 * 10 ) / 10
@@ -21,6 +34,9 @@ def angle1_to_x( angle1 ):
 def angle2_to_y( angle2 ):
     return angle1_to_x( angle2 )
     #return math.floor( angle2 * 10 ) / 10
+
+def dist_to_z( dist ):
+    return math.floor( dist ) / 10
 
 my_assert_equals( "angle1_to_x( 0 )",     angle1_to_x( 0 ),      0   )
 my_assert_equals( "angle1_to_x( -0.11 )", angle1_to_x( -0.11 ), -0.2 )
@@ -31,11 +47,11 @@ my_assert_equals( "angle1_to_x( 0.099 )", angle1_to_x( 0.099 ),  0   )
 my_assert_equals( "angle1_to_x( 0.99 )",  angle1_to_x( 0.99 ),   0.9 )
 my_assert_equals( "angle1_to_x( 1.00 )",  angle1_to_x( 1.00 ),   1.0 )
 
-def create_cube( x, y, z, listr ):
-    bpy.ops.mesh.primitive_cylinder_add(
-        radius = R, 
+def create_cube( angle1, angle2, dist, box_list ):
+    bpy.ops.mesh.primitive_cube_add(
+        radius = 0.1, 
         depth = D,
-        location = (x, y, z_offset)   
+        location = ( angle1_to_x( angle1 ), angle2_to_y( angle2 ), dist_to_z( dist ) )   
     )
     node = bpy.context.object
     node.keyframe_insert( data_path="location", frame=1 )
@@ -46,7 +62,8 @@ def create_cube( x, y, z, listr ):
     node.location[2] = z
     node.keyframe_insert( data_path="location", frame=130 )
 
-    listr.append( node )
+    all_boxes.append( node )
+    box_list.append( node )
     return node
 
 
@@ -71,7 +88,7 @@ ANGLE1 = int( 10 )
 ANGLE2 = int( 11 )
 DIST   = int( 12 )
 
-dataset = numpy.genfromtxt( "sample_data.csv", delimiter=",", skip_header=1 )
+dataset = numpy.genfromtxt( "/home/jack/HBondMachineLearning/sample_data.csv", delimiter=",", skip_header=1 )
 #print( len( dataset[ 0 ] ) )
 
 input = dataset[:,[ TX, TY, TZ, RX, RY, RZ, ANGLE1, ANGLE2, DIST ] ]
@@ -81,3 +98,6 @@ both_output = dataset[:,0:2]
 output_hbond = dataset[:,[ BEST_POSSIBLE_HBOND_SCORE  ] ]
 output_clash = dataset[:,[ WORST_POSSIBLE_CLASH_SCORE ] ]
 
+num_elements = len( dataset )
+
+# Create Boxes
