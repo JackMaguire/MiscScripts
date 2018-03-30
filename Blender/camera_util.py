779ec28a376102f5379289118303536bb0cf4549
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-30 15:22:41 -0400

    oops we need to use radians for rotation_euler

--- Blender/camera_util.py
+++ Blender/camera_util.py
@@ -31,7 +31,8 @@ def rotate_camera_around_point_XY( x, y, z, r, first_frame, last_frame, pos_offs
         camera.location[ 2 ] = z
         camera.keyframe_insert( data_path="location", frame=i )
 
-        camera.rotation_euler[ 0 ] = 90
-        camera.rotation_euler[ 1 ] = 0
-        camera.rotation_euler[ 2 ] = 360.0 * percent
+        #need to use radians here
+        camera.rotation_euler[ 0 ] = math.pi / 2.0
+        camera.rotation_euler[ 1 ] = 0.0
+        camera.rotation_euler[ 2 ] = 2.0 * math.pi * percent
         camera.keyframe_insert( data_path="rotation_euler", frame=i )
