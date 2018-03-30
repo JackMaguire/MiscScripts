a0d0e29a6760ebf93bb5dfa69038fa041fa60093
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-30 15:13:03 -0400

    off by one error...

--- Blender/camera_util.py
+++ Blender/camera_util.py
@@ -5,9 +5,10 @@ def get_active_camera():
     return bpy.context.scene.camera
 
 
+#Inclusive:
 def rotate_camera_around_point_XY( x, y, z, r, first_frame, last_frame, pos_offset ):
     camera = get_active_camera()
-    num_frames = last_frame - first_frame
+    num_frames = last_frame - first_frame + 1
     for x in range( first_frame, last_frame + 1 ):
         if( x == last_frame + 1 ):
             break#I always forget if range is inclusive
