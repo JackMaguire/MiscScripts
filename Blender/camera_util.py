60538e1b26944816a6d0a107baf14a360dc1c71b
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-30 15:18:33 -0400

    fixed duplicate variable name

--- Blender/camera_util.py
+++ Blender/camera_util.py
@@ -9,11 +9,11 @@ def get_active_camera():
 def rotate_camera_around_point_XY( x, y, z, r, first_frame, last_frame, pos_offset ):
     camera = get_active_camera()
     num_frames = last_frame - first_frame + 1
-    for x in range( first_frame, last_frame + 1 ):
-        if( x == last_frame + 1 ):
+    for i in range( first_frame, last_frame + 1 ):
+        if( i == last_frame + 1 ):
             break#I always forget if range is inclusive
 
-        pos = x + pos_offset
+        pos = i + pos_offset
         if( pos > last_frame ):
             pos -= num_frames
         pos -= first_frame
@@ -29,9 +29,9 @@ def rotate_camera_around_point_XY( x, y, z, r, first_frame, last_frame, pos_offs
         camera.location[ 0 ] = pos_x
         camera.location[ 1 ] = pos_y
         camera.location[ 2 ] = z
-        camera.keyframe_insert( data_path="location", frame=x )
+        camera.keyframe_insert( data_path="location", frame=i )
 
         camera.rotation_euler[ 0 ] = 90
         camera.rotation_euler[ 1 ] = 0
         camera.rotation_euler[ 2 ] = 360.0 * percent
-        camera.keyframe_insert( data_path="rotation_euler", frame=x )
+        camera.keyframe_insert( data_path="rotation_euler", frame=i )
