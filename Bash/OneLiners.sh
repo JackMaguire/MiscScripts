a66aaeeab8c1a07b7395ec4ca74e59d88b5edcd7
Jack Maguire <jackmaguire1444@gmail.com>
2018-04-24 14:21:09 -0400

    Added line for mpi gdb over ssh using xterm

--- Bash/OneLiners.sh
+++ Bash/OneLiners.sh
@@ -3,3 +3,6 @@ exit( 1 ) #just making sure we never run this
 
 #get min and max of csv column
 awk -F, -v min=100 -v max=0 '{if($10<min){min=$10}}{if($10>max){max=$10}}END{print min " " max} ' filename
+
+#not really bash, but run MPI GDB over SSH
+mpirun -n 20 xterm -e gdb -ex run -x gdb.txt --args [executable with args]
