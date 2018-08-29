9f81b9c700a7726cf04715d88deaa0e60431ddd8
Jack Maguire <jack@med.unc.edu>
2018-08-29 12:04:56 -0400

    plot seems to work now, but still fine tuning

--- Python/contour_plot_from_x_y_csv.py
+++ Python/contour_plot_from_x_y_csv.py
@@ -1,7 +1,13 @@
-from numpy import linspace, meshgrid
-import matplotlib.pyplot as plt
 from matplotlib.mlab import griddata
+from numpy import linspace, meshgrid
+from numpy.random import uniform, seed
+from scipy.interpolate import griddata
 import csv
+import matplotlib.cm as cm
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.ma as ma
 
 def grid(x, y, z, resX=100, resY=100):
     "Convert 3 column data to matplotlib grid"
@@ -13,7 +19,7 @@ def grid(x, y, z, resX=100, resY=100):
 
 x_array = []
 y_array = []
-z_array = []
+#z_array = []
 
 f=open("don_and_acceptor_only.exposed.csv", "r")
 contents = f.readlines()
@@ -22,9 +28,72 @@ for i in contents:
     y = float( i.split('\n')[0].split(',')[1] )
     x_array.append( x )
     y_array.append( y )
-    z_array.append( 0 )
+    #z_array.append( 0 )
     #print( str(x) + " ... " + str(y) )
 
-plt.scatter( x_array, y_array, z_array )
-#X, Y, Z = grid(x, y, z)
-#plt.contourf(X, Y, Z)
+print( len( x_array ) )
+
+#plt.scatter( x_array, y_array )
+#X, Y = grid(x_array, y_array)
+#plt.contourf(X, Y)
+#plt.show()
+#plt.savefig('plot.png')
+
+'''
+delta = 0.1
+x = np.arange(0, 100, delta)
+y = np.arange(0, 100, delta)
+X, Y = np.meshgrid(x, y)
+Z1 = mlab.bivariate_normal(X, Y, 1.0, 1.0, 0.0, 0.0)
+Z2 = mlab.bivariate_normal(X, Y, 1.5, 0.5, 1, 1)
+Z = 10.0 * (Z2 - Z1)
+
+norm = cm.colors.Normalize(vmax=abs(Z).max(), vmin=-abs(Z).max())
+cmap = cm.PRGn
+
+levels = np.arange(-2.0, 1.601, 0.4)
+
+fig, axes = plt.subplots(1,1, sharey=True)
+
+ax = axes
+
+ax.contourf(X, Y, Z, levels,
+            cmap=cm.get_cmap(cmap, len(levels)-1),
+            norm=norm)
+ax.autoscale(False) # To avoid that the scatter changes limits
+ax.scatter( x_array,
+            y_array,
+            zorder=1)
+ax.set_title('Scatter with zorder={0}'.format(1))
+
+plt.show()
+'''
+
+# make up some randomly distributed data
+seed(1234)
+npts = len(x_array)
+#x = uniform(-2,2,len(x_array))
+#y = uniform(-2,2,len(y_array))
+x = np.asarray( x_array )
+print( len( x ) )
+assert len( x ) == len( x_array )
+y = np.asarray( y_array )
+assert len( y ) == len( y_array )
+z = x * np.exp( -x**2 - y**2 )
+print( len( z ) )
+# define grid
+xi = np.linspace(0,100,1000)
+yi = np.linspace(0,200,1000)
+# grid the data.
+#zi = xi*yi
+zi = griddata((x, y), z, (xi[None,:], yi[:,None]), method='cubic')
+# contour the gridded data, plotting dots at the randomly spaced data points.
+CS = plt.contour(xi,yi,zi,15,linewidths=0.5,colors='k')#ValueError: zero-size array to reduction operation minimum which has no identity
+CS = plt.contourf(xi,yi,zi,15,cmap=plt.cm.jet)
+plt.colorbar() # draw colorbar
+# plot data points.
+plt.scatter(x,y,marker='o',c='b',s=5)
+plt.xlim(0,100)
+plt.ylim(0,200)
+plt.title('griddata test (%d points)' % npts)
+plt.show()
